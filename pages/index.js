7d869f32d50dc7254bd22bcb47e722e27de89db1
mrpp2001 <pratiknpatil2001@gmail.com>
2023-11-02 14:34:00 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,7 @@ export default function App() {
   return (
     <ul>
       <li>
-        <Link href="/car">ThreeJS Car</Link>
-      </li>
-      <li>
-        <Link href="/intro">ThreeJS Scooter</Link>
-      </li>
-      <li>
-        <Link href="/markers">Advanced Markers</Link>
+        <Link href="/human">ThreeJS Human</Link>
       </li>
     </ul>
   );
